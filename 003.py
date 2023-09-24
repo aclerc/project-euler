64f15f03dd0c29fbd225cff44118b7d685ab25db
Alex Clerc <alex.clerc@res-group.com>
2023-09-24 10:20:00 +0100

    problems 1 to 9

---
+++ 003.py
@@ -0,0 +1,41 @@
+import time
+start = time.time()
+
+def is_prime(n):
+    if n < 2:
+        return False
+    else:
+        prime = True
+        f = 2
+        while f <= n // f:
+            prime = n % f != 0
+            if not prime:
+                break
+            f += 1
+        return prime
+
+def get_next_prime(n):
+    if n < 2:
+        return 2
+    else:
+        p = n + (n % 2 + 1)
+        while not is_prime(p):
+            p += 2
+        return p
+
+
+# n = 13195
+n = 600851475143
+# n = 13195 * 15487457
+print(f"What is the largest prime factor of the number {n}?")
+this_prime = 2
+while not is_prime(n):
+    if n % this_prime == 0:
+        print(f"{this_prime} divides {n}")
+        n = n // this_prime
+    else:
+        this_prime = get_next_prime(this_prime)
+    if this_prime > (n ** 0.5):
+        break
+print(f"answer is {n}")
+print(f"time taken = {time.time()-start:.4f}s")
---
+++ 009.py
@@ -0,0 +1,30 @@
+import time
+start = time.time()
+
+print("There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc.")
+
+
+answer_found = False
+
+#guess a value of c
+c = 500
+d = 0
+s = 1
+while not answer_found:
+    c += (s*d)
+    d += 1
+    s *= -1
+    a = 1
+    b = 1000 - c - a
+    while (a <= b) and not answer_found:
+        if c ** 2 == a ** 2 + b ** 2:
+            answer_found = True
+        else:
+            a += 1
+            b -= 1
+
+print(f"a={a}")
+print(f"b={b}")
+print(f"c={c}")
+print(f"answer: {a*b*c}")
+print(f"time taken = {time.time()-start:.4f}s")
